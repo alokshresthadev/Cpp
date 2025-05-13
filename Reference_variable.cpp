cad0a525a231a81d48d97e29211f5660b4135d8e
alokshresthadev <alokshresthadev@gmail.com>
2025-05-13 13:51:47 +0545

    reference variable

---
+++ Reference_variable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    int &b = a;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    a = 10;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    b=2;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
